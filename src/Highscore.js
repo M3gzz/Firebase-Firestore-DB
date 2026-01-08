a3751751a34a6e265b0a55791870b25b9243a06a
Megan <m3gzzcsgo@gmail.com>
2026-01-08 09:20:32 +0200

    Just cleaned up the code

--- src/Highscore.js
+++ src/Highscore.js
@@ -1,39 +1,33 @@
 import { useState, useEffect } from "react";
-import {getHighscore, uploadHighscore} from './firebase_firestore';
+import { getHighscore, uploadHighscore } from "./firebase_firestore";
 
 function Highschore() {
+  const [currentHighscore, setCurrentHighscore] = useState(0);
 
-    const [currentHighscore, setCurrentHighscore] = useState(0);
+  useEffect(() => {
+    const getData = async () => {
+      let result = await getHighscore();
+      setCurrentHighscore(result);
+    };
 
-    useEffect(() => {
-        
-        const getData = async () => {
-            let result = await getHighscore();
-            setCurrentHighscore(result);
-        }
-        
-        getData();
+    getData();
+  }, []);
 
-    }, [])
+  const handleFormSubmission = async (event) => {
+    event.preventDefault();
+    let result = await uploadHighscore(event.target.newScore.value);
+    setCurrentHighscore(event.target.newScore.value);
+  };
 
-
-       const handleFormSubmission = async (event) => {
-        event.preventDefault(); 
-        let result = await uploadHighscore(event.target.newScore.value);
-        setCurrentHighscore(event.target.newScore.value);
-       } 
-
-
-
-    return (
-        <div>
-            <h3>Current Highscore {currentHighscore}</h3>
-            <form onSubmit={handleFormSubmission}>
-                <input type='number'  id='newScore' placeholder={0} />
-                <button type='submit'>Upload New Highscore</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h3>Current Highscore {currentHighscore}</h3>
+      <form onSubmit={handleFormSubmission}>
+        <input type="number" id="newScore" placeholder={0} />
+        <button type="submit">Upload New Highscore</button>
+      </form>
+    </div>
+  );
 }
 
 export default Highschore;
